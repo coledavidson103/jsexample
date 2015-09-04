438671728e3bf01053d74acfef825baf3aa70f3c
Cole Davidson <coledavidson@Coles-MacBook-Pro.local>
2015-09-04 12:56:41 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.blue').on('click', function () {
+		$('.box').toggleClass('blue'),
+		$('.box').text("Now it's blue")
+	}
+);
+
+$('.red').on('click', function () {
+		$('.box').toggleClass('red')
+		$('.box').text("Now it's red")
+	}
+);
+
+$('.green').on('click', function () {
+		$('.box').toggleClass('green')
+		$('.box').text("Now it's green")
+	}
+);
